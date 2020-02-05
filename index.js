81df76750e6de6cf6e931093602c6948461fe8a5
Avera Guthrie <averaguthrie@gmail.com>
2020-02-05 20:10:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for(let i = 0; i < musicians.length; i++) {
-    console.log(musicians[i])
+    array.push(`${musicians} plays ${instruments}`)
   }
+  return array;
 }
